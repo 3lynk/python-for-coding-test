ddb48121c7cc150742313efb1759d0e87c9a25b5
Geonmin <kimgunmin77@gmail.com>
2024-06-03 23:05:57 +0900

    Update draft.py

--- Binary Search/#3/draft.py
+++ Binary Search/#3/draft.py
@@ -1,3 +1,41 @@
+# sum length
+def sum_length(H) -> int:
+  result = 0
+
+  for i in reversed(length):
+    if i - H <= 0:
+      break
+    result += (i - H)
+
+  return result
+
+
+# input N, M, length
+N, M = map(int, input().split())
+length = sorted(map(int, input().split()))
+
+# init start, end, ans
+start, end = 0, length[-1]
+ans = []
+
+# binary search with while
+while start <= end:
+  mid = (end + start) // 2
+
+  result = sum_length(mid)
+
+  if result == M:
+    ans.append(mid)
+    break
+  if result < M:
+    end = mid - 1
+  elif result > M:
+    start = mid + 1
+    ans.append(mid)
+
+# print ans
+print(ans[-1])
+
 '''memo
 Input
 
